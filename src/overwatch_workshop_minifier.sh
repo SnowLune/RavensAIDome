d931a006da6f1e53c0552f6e29caa629af2bcba0
Luna R <snowluna@protonmail.com>
2022-02-03 01:20:36 +0000

    delete mode file, update Makefile to use new minify script

--- src/overwatch_workshop_minifier.sh
+++ src/overwatch_workshop_minifier.sh
@@ -2,7 +2,7 @@
 # Minify Overwatch Workshop scripts from stdin or file
 
 if [ $# -eq 0 ]; then
-   echo "No arguments given. Using stdin..."
+   # No arguments given. Using stdin...
    sed 's/\s\+/ /g' | tr -ds '\r''\n' '[:blank:]'
 
 else
